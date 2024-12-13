8ea694d94e8448177104f0333130d46b3a0689b0
Tasos Papalyras <tasos@rdnt.dev>
2024-12-13 13:55:59 +0200

    Driver refactor, optimizations toggle, additional tests & support for all SKUs

--- sharpmem/sharpmem.go
+++ sharpmem/sharpmem.go
@@ -1,41 +1,57 @@
-//go:build tinygo
-
 package sharpmem
 
 import (
 	"image/color"
-	"machine"
 
 	"tinygo.org/x/drivers"
 )
 
 const (
-	bitWriteCmd = 0x01
-	bitVcom     = 0x02
-	bitClear    = 0x04
+	bitWriteCmd uint8 = 0x01
+	bitVcom     uint8 = 0x02
+	bitClear    uint8 = 0x04
 )
 
-// Device represents a Sharp Memory Display device.
+type Pin interface {
+	High()
+	Low()
+}
+
+// Device represents a Sharp Memory Display device. This driver implementation
+// concerns the 1-bit color versions only (black and white memory displays).
+//
+// Supported SKUs include:
+// LS010B7DH04, LS011B7DH03, LS012B7DD01, LS013B7DH03, LS013B7DH05,
+// LS018B7DH02, LS027B7DH01, LS027B7DH01A, LS032B7DD02, LS044Q7DH01
+//
+// The driver includes optimizations (frame and per-line invalidation) that
+// only transmit the changed lines to the display. These optimizations are on
+// by default, and they can be disabled with the respective Config option.
 type Device struct {
-	bus        drivers.SPI
-	csPin      machine.Pin
-	buffer     []byte
-	height     int16
-	width      int16
-	bufferSize int16
-	lineDiff   []byte
-	lineBuf    []byte
-	vcom       uint8
+	bus          drivers.SPI
+	csPin        Pin
+	buffer       []byte
+	txBuf        []byte
+	lineDiff     []byte
+	width        int16
+	height       int16
+	bufferSize   int16
+	bytesPerLine int16
+	vcom         uint8
+	diffing      bool
 }
 
 type Config struct {
 	Width  int16
 	Height int16
+
+	// DisableOptimizations disables frame and line invalidation diff.
+	DisableOptimizations bool
 }
 
-// NewSPI creates a new connection.
+// New creates a new device connection.
 // The SPI bus must have already been configured.
-func NewSPI(bus drivers.SPI, csPin machine.Pin) Device {
+func New(bus drivers.SPI, csPin Pin) Device {
 	d := Device{
 		bus:   bus,
 		csPin: csPin,
@@ -43,18 +59,19 @@ func NewSPI(bus drivers.SPI, csPin machine.Pin) Device {
 	return d
 }
 
-// Configure initializes the display with specified configuration.
+// Configure initializes the display with specified configuration. It can be
+// called multiple times on the same display, resetting its internal state.
 func (d *Device) Configure(cfg Config) {
-	if cfg.Width != 0 {
-		d.width = cfg.Width
-	} else {
-		d.width = 128
+	if cfg.Width == 0 {
+		cfg.Width = 160
 	}
-	if cfg.Height != 0 {
-		d.height = cfg.Height
-	} else {
-		d.height = 64
+	if cfg.Height == 0 {
+		cfg.Height = 68
 	}
+
+	d.width = cfg.Width
+	d.height = cfg.Height
+	d.diffing = !cfg.DisableOptimizations
 
 	d.initialize()
 }
@@ -63,36 +80,43 @@ func (d *Device) Configure(cfg Config) {
 func (d *Device) initialize() {
 	d.csPin.Low()
 
+	// initialize VCOM as high
 	d.vcom = bitVcom
 
-	d.bufferSize = d.width * d.height / 8
+	// bytesPerLine has to be 16-bit aligned, as some resolutions require
+	// padding to the nearest 2nd byte.
+	d.bytesPerLine = ceilDiv(d.width, 16) * 2
+
+	// preallocate a contiguous byte buffer for all lines, including
+	// protocol-required padding for each line apriori (easier to transfer).
+	d.bufferSize = d.bytesPerLine * d.height
 	d.buffer = make([]byte, d.bufferSize)
-
-	d.lineDiff = make([]byte, bitfieldBufLen(d.height))
-
 	for i := range d.buffer {
 		d.buffer[i] = 0xff
 	}
 
-	bytesPerLine := d.width / 8
+	// auxiliary buffer for SPI transfers to avoid dynamic allocations
+	d.txBuf = make([]byte, 2)
 
-	d.lineBuf = make([]byte, bytesPerLine+2)
+	if d.diffing {
+		// buffer to store the changed lines. First *bit* is whether any line has
+		// changed at all, followed by N bits, one for each line.
+		d.lineDiff = make([]byte, bitfieldBufLen(1+d.height))
+	}
 }
 
 // SetPixel enables or disables a pixel in the buffer
-// color.RGBA{0, 0, 0, 255} is considered transparent (white), anything else
-// will enable a pixel on the screen, making it appear less reflective (black).
+// color.RGBA{0, 0, 0, 255} is considered transparent (reflective, white),
+// anything else will enable a pixel on the screen (make it appear less
+// reflective, black).
 func (d *Device) SetPixel(x, y int16, c color.RGBA) {
-	bitPos := x + y*d.width
+	offset := y * d.bytesPerLine
 
-	div := bitPos / 8
-	mod := uint8(bitPos % 8)
+	div := offset + x/8
+	mod := uint8(x % 8)
 
 	prev := hasBit(d.buffer[div], mod)
-	curr := true
-	if c.R == 0 && c.G == 0 && c.B == 0 && c.A == 255 {
-		curr = false
-	}
+	curr := c.R == 0 && c.G == 0 && c.B == 0 && c.A == 255
 
 	if prev == curr {
 		return
@@ -104,9 +128,14 @@ func (d *Device) SetPixel(x, y int16, c color.RGBA) {
 		d.buffer[div] = unsetBit(d.buffer[div], mod)
 	}
 
-	linediv := y / 8
-	linemod := uint8(y % 8)
-	d.lineDiff[linediv] = setBit(d.lineDiff[linediv], linemod)
+	if d.diffing {
+		// mark the frame as invalidated
+		d.lineDiff[0] = setBit(d.lineDiff[0], 0)
+
+		linediv := (y + 1) / 8
+		linemod := uint8((y + 1) % 8)
+		d.lineDiff[linediv] = setBit(d.lineDiff[linediv], linemod)
+	}
 }
 
 // Size returns the current size of the display.
@@ -114,62 +143,92 @@ func (d *Device) Size() (x, y int16) {
 	return d.width, d.height
 }
 
-// Display sends the whole buffer to the screen. If a line hasn't changed,
-// it will not be transferred. It should ideally be called at >=1hz.
+// Display renders the buffer to the screen. It only transmits changed lines if
+// optimizations are enabled. It should be called at >=1hz, even if the
+// buffer hasn't been modified.
 func (d *Device) Display() error {
-	defer func() {
-		for i := 0; i < len(d.lineDiff); i++ {
-			d.lineDiff[i] = 0x00
+	if d.diffing {
+		if !hasBit(d.lineDiff[0], 0) {
+			// no pixels have been modified, simply toggle VCOM
+			return d.holdDisplay()
 		}
-	}()
+
+		defer func() {
+			for i := 0; i < len(d.lineDiff); i++ {
+				d.lineDiff[i] = 0x00
+			}
+		}()
+	}
 
 	// start transfer
 	d.csPin.High()
 
-	// send write command
-	_, err := d.bus.Transfer(d.vcom | bitWriteCmd)
+	cmd := bitWriteCmd | d.vcom
+
+	d.toggleVcom()
+
+	for i := int16(0); i < d.height; i++ {
+		if d.diffing {
+			// Skip rendering lines that haven't changed.
+			linediv := (i + 1) / 8
+			linemod := uint8((i + 1) % 8)
+			if !hasBit(d.lineDiff[linediv], linemod) {
+				continue
+			}
+		}
+
+		// The first 5 bits are either dummy or part of the current line
+		// (1-indexed) if it overflows 8-bits.
+		// The last 3 bits are the command for the first line and dummy bits
+		// for subsequent lines (set as command for simplicity)
+		d.txBuf[0] = cmd | uint8((i+1)>>8<<3)
+
+		// The second byte is the low bits of the current line (1-indexed).
+		// for <8 bits cases, the high bits are dummy, so we leave them as 0.
+		d.txBuf[1] = uint8(i + 1)
+
+		// send the first two bytes
+		err := d.bus.Tx(d.txBuf, nil)
+		if err != nil {
+			return err
+		}
+
+		// send the line data
+		err = d.bus.Tx(d.buffer[i*d.bytesPerLine:(i+1)*d.bytesPerLine], nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Trailer 16 bits (low)
+	d.txBuf[0] = 0x00
+	d.txBuf[1] = 0x00
+	err := d.bus.Tx(d.txBuf, nil)
+	if err != nil {
+		return err
+	}
+
+	// end transfer
+	d.csPin.Low()
+
+	return nil
+}
+
+// holdDisplay simply toggles VCOM without updating any lines.
+func (d *Device) holdDisplay() error {
+	// begin transaction
+	d.csPin.High()
+
+	d.txBuf[0] = d.vcom
+	d.txBuf[1] = 0x00
+	err := d.bus.Tx(d.txBuf, nil)
 	if err != nil {
 		return err
 	}
 
 	d.toggleVcom()
 
-	bytesPerLine := d.width / 8
-	totalBytes := (d.width * d.height) / 8
-
-	for i := int16(0); i < totalBytes; i += bytesPerLine {
-		// first byte is current line (0-indexed)
-		currentLine := (i + 1) / (d.width / 8)
-
-		// skip rendering lines that haven't changed
-		linediv := (currentLine) / 8
-		linemod := uint8((currentLine) % 8)
-		if !hasBit(d.lineDiff[linediv], linemod) {
-			continue
-		}
-
-		d.lineBuf[0] = uint8(currentLine) + 1 // encode as 1-indexed
-
-		// data bytes (copy to lineBuf to avoid modifications to buffer)
-		copy(d.lineBuf[1:bytesPerLine+1], d.buffer[i:i+bytesPerLine])
-
-		// last byte is always 0x00
-		d.lineBuf[bytesPerLine+1] = 0x00
-
-		// send the line data
-		err = d.bus.Tx(d.lineBuf, nil)
-		if err != nil {
-			return err
-		}
-	}
-
-	// trailer byte (always 0x00)
-	_, err = d.bus.Transfer(0x00)
-	if err != nil {
-		return err
-	}
-
-	// end transfer
+	// end transaction
 	d.csPin.Low()
 
 	return nil
@@ -184,44 +243,48 @@ func (d *Device) Clear() error {
 // ClearBuffer clears the in-memory buffer. The display is not updated.
 func (d *Device) ClearBuffer() {
 	// detect what rows need to be reset on the next render
-	for y := int16(0); y < d.height; y++ {
-		invalidateRow := false
-		for x := int16(0); x < d.width; x++ {
-			bitPos := x + y*d.width
+	if d.diffing {
+		for y := int16(0); y < d.height; y++ {
+			offset := y * d.bytesPerLine
 
-			div := bitPos / 8
-			mod := uint8(bitPos % 8)
+			updateLine := false
+			for x := int16(0); x < d.width; x++ {
+				div := offset + x/8
+				mod := uint8(x % 8)
 
-			if !hasBit(d.buffer[div], mod) {
-				invalidateRow = true
-				break
+				if !hasBit(d.buffer[div], mod) {
+					updateLine = true
+					break
+				}
 			}
-		}
 
-		if invalidateRow {
-			linediv := y / 8
-			linemod := uint8(y % 8)
-			d.lineDiff[linediv] = setBit(d.lineDiff[linediv], linemod)
+			if updateLine {
+				// mark the frame as invalidated
+				d.lineDiff[0] = setBit(d.lineDiff[0], 0)
+
+				linediv := (y + 1) / 8
+				linemod := uint8((y + 1) % 8)
+				d.lineDiff[linediv] = setBit(d.lineDiff[linediv], linemod)
+			}
 		}
 	}
 
-	// reset in-memory buffer
+	// reset in-memory buffer to high
 	for i := 0; i < len(d.buffer); i++ {
 		d.buffer[i] = 0xff
 	}
 }
 
-// ClearDisplay clears the display. The in-memory buffer is not updated.
+// ClearDisplay clears the display. The in-memory buffer is not updated. A
+// subsequent call to Display() will re-render the content as it was before
+// clearing.
 func (d *Device) ClearDisplay() error {
 	// begin transaction
 	d.csPin.High()
 
-	b := []byte{
-		d.vcom | bitClear,
-		0x00,
-	}
-
-	err := d.bus.Tx(b, nil)
+	d.txBuf[0] = d.vcom | bitClear
+	d.txBuf[1] = 0x00
+	err := d.bus.Tx(d.txBuf, nil)
 	if err != nil {
 		return err
 	}
--- sharpmem/util_test.go
+++ sharpmem/util_test.go
@@ -1,6 +1,8 @@
 package sharpmem
 
 import (
+	"image/color"
+	"math/rand/v2"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -42,7 +44,7 @@ func Test_hasBit(t *testing.T) {
 func Test_bitfieldBufLen(t *testing.T) {
 	c := qt.New(t)
 
-	for i := 1; i < 536; i++ {
+	for i := int16(1); i < 536; i++ {
 		requiredBufferSize := i / 8
 		wouldOverflow := i % 8
 
@@ -50,6 +52,80 @@ func Test_bitfieldBufLen(t *testing.T) {
 			requiredBufferSize += 1
 		}
 
-		c.Assert(bitfieldBufLen(int16(i)), qt.Equals, int16(requiredBufferSize))
+		c.Assert(bitfieldBufLen(i), qt.Equals, requiredBufferSize)
+	}
+}
+
+type mockBus struct{}
+
+func (m mockBus) Tx(w, r []byte) error {
+	return nil
+}
+
+func (m mockBus) Transfer(b byte) (byte, error) {
+	return b, nil
+}
+
+type mockPin struct{}
+
+func (m mockPin) High() {
+}
+
+func (m mockPin) Low() {
+}
+
+func Test_Device(t *testing.T) {
+	c := qt.New(t)
+
+	cfgs := []Config{
+		{Width: 128, Height: 128}, // LS010B7DH04, LS013B7DH03
+		{Width: 160, Height: 68},  // LS011B7DH03
+		{Width: 184, Height: 38},  // LS012B7DD01
+		{Width: 144, Height: 168}, // LS013B7DH05
+		{Width: 230, Height: 303}, // LS018B7DH02
+		{Width: 400, Height: 240}, // LS027B7DH01, LS027B7DH01A
+		{Width: 336, Height: 536}, //LS032B7DD02
+		{Width: 320, Height: 240}, //LS044Q7DH01
+	}
+
+	cfgLen := len(cfgs)
+	for i := 0; i < cfgLen; i++ {
+		cfgs = append(cfgs, Config{
+			Width:                cfgs[i].Width,
+			Height:               cfgs[i].Height,
+			DisableOptimizations: true,
+		})
+	}
+
+	spi := mockBus{}
+	pin := mockPin{}
+	display := New(spi, pin)
+
+	for _, cfg := range cfgs {
+		display.Configure(cfg)
+
+		x, y := display.Size()
+		c.Assert(x, qt.Equals, cfg.Width)
+		c.Assert(y, qt.Equals, cfg.Height)
+
+		for i := 0; i < 10; i++ {
+			x := int16(rand.IntN(int(cfg.Width)))
+			y := int16(rand.IntN(int(cfg.Height)))
+			display.SetPixel(x, y, color.RGBA{R: 255, G: 255, B: 255, A: 255})
+		}
+
+		for i := 0; i < 10; i++ {
+			x := int16(rand.IntN(int(cfg.Width)))
+			y := int16(rand.IntN(int(cfg.Height)))
+			display.SetPixel(x, y, color.RGBA{R: 0, G: 0, B: 0, A: 255})
+		}
+
+		err := display.Display()
+		c.Assert(err, qt.Equals, nil)
+
+		err = display.ClearDisplay()
+		c.Assert(err, qt.Equals, nil)
+
+		display.ClearBuffer()
 	}
 }
